1605335bcbbb3248e51487b094824914bcd71774
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2018-03-08 17:54:09 +0530

    First commit

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,20 @@
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  canActivate(route, state: RouterStateSnapshot) {
+    if (this.authService.isLoggedIn()) { return true };
+
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
   constructor(private http: Http) {
   }
 
-  login(credentials) { 
-   return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+  login(credentials) {
+   return this.http.post('/api/authenticate',
+      JSON.stringify(credentials))
+      .map(response => {
+        const result = response.json();
+        if (result && result.token)  {
+          localStorage.setItem('token', result.token);
+          return true;
+        }
+        return false;
+      });
   }
 
-  logout() { 
+  logout() {
+    localStorage.removeItem('token');
   }
 
-  isLoggedIn() { 
-    return false;
+  isLoggedIn() {
+    return tokenNotExpired();
+  }
+
+  get currentUser() {
+    const token = localStorage.getItem('token');
+    if (!token) { return null };
+
+    return new JwtHelper().decodeToken(token);
   }
 }
 
